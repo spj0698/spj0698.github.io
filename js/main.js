cd5c23b04889d575f0d2707d2febdeedace584e0
spj0698 <63954692+spj0698@users.noreply.github.com>
2020-04-20 03:20:08 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,7 +9,49 @@ $('#navbar a, .btn').on('click', function(event) {
             {
                 scrollTop: $(hash).offset().top - 80
             },
-            800
+            800 //800ms
         );
     }
 });
+
+$(document).ready(function() {
+    $('.submit').click(function (event) {
+      event.preventDefault()
+      console.log('Clicked Button')
+      
+      var email = $('.email').val()
+      var firstname = $('.firstname').val()
+      var lastname = $('.lastname').val()
+      var message = $('.message').val()
+      var statusElm = $('.status')
+      statusElm.empty()
+      
+      if (email.length > 5 && email.includes('@') && email.includes('.')) {
+        statusElm.append('<div>Email is valid</div>')
+      } else {
+        event.preventDefault()
+        statusElm.append('<div>Email is not valid</div>')
+      }
+      
+      if(message.length >= 2) {
+        statusElm.append('<div>Subject is valid</div>')
+      } else {
+        event.preventDefault()
+        statusElm.append('<div>Subject is not valid</div>')
+      }
+      
+      if(firstname.length >= 1) {
+        statusElm.append('<div>First Name is valid</div>')
+      } else {
+        event.preventDefault()
+        statusElm.append('<div>First Name is not valid</div>')
+      }
+
+      if(lastname.length >= 1) {
+        statusElm.append('<div>Last Name is valid</div>')
+      } else {
+        event.preventDefault()
+        statusElm.append('<div>last Name is not valid</div>')
+      }
+    })
+  })
